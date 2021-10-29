f3154b6184dc992cb49db5943426f006aa594bd0
Johanna Raisa <johanna.raisa@gmail.com>
2021-10-29 09:31:27 +0300

    Add noticeheader css

--- Koha/Plugin/Fi/KohaSuomi/PrintPdfNotices.pm
+++ Koha/Plugin/Fi/KohaSuomi/PrintPdfNotices.pm
@@ -10,7 +10,7 @@ use C4::Context;
 use utf8;
 
 ## Here we set our plugin version
-our $VERSION = "1.0.1";
+our $VERSION = "1.0.2";
 
 ## Here is our metadata, some keys are required, some are optional
 our $metadata = {
