a4218b199096c4353d82366dfa608f6b7bb12d11
Johanna Raisa <johanna.raisa@gmail.com>
2022-05-13 12:44:25 +0300

    Update set-development

--- set-development.sh
+++ set-development.sh
@@ -1,21 +1,15 @@
 #!/bin/bash
 
 kohaplugindir="$(grep -Po '(?<=<pluginsdir>).*?(?=</pluginsdir>)' $KOHA_CONF)"
+kohadir="$(grep -Po '(?<=<intranetdir>).*?(?=</intranetdir>)' $KOHA_CONF)"
 
 rm -r $kohaplugindir/Koha/Plugin/Fi/KohaSuomi/PrintPdfNotices
 rm $kohaplugindir/Koha/Plugin/Fi/KohaSuomi/PrintPdfNotices.pm
 
-ln -s "/home/jraisa//koha-plugin-print-pdf-notices/Koha/Plugin/Fi/KohaSuomi/PrintPdfNotices" $kohaplugindir/Koha/Plugin/Fi/KohaSuomi/PrintPdfNotices
-ln -s "/home/jraisa//koha-plugin-print-pdf-notices/Koha/Plugin/Fi/KohaSuomi/PrintPdfNotices.pm" $kohaplugindir/Koha/Plugin/Fi/KohaSuomi/PrintPdfNotices.pm
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-DATABASE=`xmlstarlet sel -t -v 'yazgfs/config/database' $KOHA_CONF`
-HOSTNAME=`xmlstarlet sel -t -v 'yazgfs/config/hostname' $KOHA_CONF`
-PORT=`xmlstarlet sel -t -v 'yazgfs/config/port' $KOHA_CONF`
-USER=`xmlstarlet sel -t -v 'yazgfs/config/user' $KOHA_CONF`
-PASS=`xmlstarlet sel -t -v 'yazgfs/config/pass' $KOHA_CONF`
+ln -s "$SCRIPT_DIR/Koha/Plugin/Fi/KohaSuomi/PrintPdfNotices" $kohaplugindir/Koha/Plugin/Fi/KohaSuomi/PrintPdfNotices
+ln -s "$SCRIPT_DIR/Koha/Plugin/Fi/KohaSuomi/PrintPdfNotices.pm" $kohaplugindir/Koha/Plugin/Fi/KohaSuomi/PrintPdfNotices.pm
 
-mysql --user=$USER --password="$PASS" --port=$PORT --host=$HOST $DATABASE << END
-DELETE FROM plugin_data where plugin_class = 'Koha::Plugin::Fi::KohaSuomi::PrintPdfNotices';
-INSERT INTO plugin_data (plugin_class,plugin_key,plugin_value) VALUES ('Koha::Plugin::Fi::KohaSuomi::PrintPdfNotices','__INSTALLED__','1');
-END
+perl $kohadir/misc/devel/install_plugins.pl
 
